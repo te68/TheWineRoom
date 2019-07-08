2dd36aec49c7a46636c30095beb6a9b934a02a1d
te68 <te68@cornell.edu>
2019-07-08 17:18:12 -0400

    developed users controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.delete_all()
-Wine.delete_all()
+CartWine.delete_all()
 Review.delete_all()
 Cart.delete_all()
-CartWine.delete_all()
+User.delete_all()
+Wine.delete_all()
 
 
 
-User.create(name: "Matt", password: "password", preferences: {grape: ["reisling"], price: 15, min_rating: 3})
 
-Wine.create(name: "Austin Hope", grape: "Cabernet Sauvginon", price: 30, rating: 4.5, properties: {red_white: "red", sweet_dry: "dry", country: "United States", pairings: ["prime rib"], year: 2010})
 
-Review.create(rating: 5, content: "Smoky", wine_id: Wine.find_by(name: "Austin Hope").id, user_id: User.find_by(name: 'Matt').id)
+User.create!(name: "Matt", password: "password", preferences: {grape: ["reisling"], price: 15, min_rating: 3})
 
-Cart.create(user_id: User.find_by(name: "Matt").id)
+Wine.create!(name: "Austin Hope", grape: "Cabernet Sauvginon", price: 30, rating: 4.5, properties: {red_white: "red", sweet_dry: "dry", country: "United States", pairings: ["prime rib"], year: 2010})
 
-CartWine.create(wine_id: Wine.find_by(name: "Austin Hope").id, cart_id: Cart.find_by(user_id: User.find_by(name: "Matt")).id)
+Review.create!(rating: 5, content: "Smoky", wine_id: Wine.find_by(name: "Austin Hope").id, user_id: User.find_by(name: 'Matt').id)
+
+Cart.create!(user_id: User.find_by(name: "Matt").id)
+
+CartWine.create!(wine_id: Wine.find_by(name: "Austin Hope").id, cart_id: Cart.find_by(user_id: User.find_by(name: "Matt")).id)
