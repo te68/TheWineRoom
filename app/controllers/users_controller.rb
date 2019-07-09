b69f10a78fa1f440ca60deb9a3e034cbddb4d51b
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-09 10:47:13 -0400

    Added user pages: new, show, edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @grapes = ["Cabernet Sauvginon", "Merlot", "Sauvginon Blanc", "Pinot Noir", "Reisling", "Malbec", "Pinot Grigio"]
+    @user = User.new
+  end
+
+  def create
+    name = params[:user][:name]
+    password = params[:user][:password]
+    grape = params[:preferences][:grape]
+    min_rating = params[:preferences][:min_rating]
+    price = params[:preferences][:price]
+    @user = User.create(name: name, password: password, preferences: { grape: grape, min_rating: min_rating, price: price })
+    redirect_to user_path(@user)
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -14,6 +29,24 @@ class UsersController < ApplicationController
   end
 
   def update
-    byebug
+    @user = User.find(params[:id])
+    if params[:user][:preferences].class == Array
+      params[:user][:preferences].each do |grape|
+        @user.preferences[:grape] << grape
+      end
+    elsif params[:user][:preferences].to_i > 5
+      @user.preferences[:price] = params[:user][:preferences].to_i
+    else
+      @user.preferences[:min_rating] = params[:user][:preferences]
+    end
+    @user.preferences[:grape].uniq
+    @user.save
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :preferences)
   end
 end
