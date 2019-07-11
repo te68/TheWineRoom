41f81ae43aee3487a280d5efb3136869d8b0a04f
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-11 17:03:18 -0400

    Did a lot of css,filter left

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,6 +1,12 @@
 class WinesController < ApplicationController
   def index
+    @grapes = ["Cabernet Sauvginon", "Merlot", "Sauvginon Blanc", "Pinot Noir", "Reisling", "Malbec", "Pinot Grigio"]
     @wines = Wine.all
+    if params[:country] == nil
+      @wines = Wine.all[0..100]
+    else
+      @wines = Heroine.all.select { |heroine| heroine.power_id == params[:power_id].to_i }
+    end
   end
 
   def show
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,3 +1,4 @@
 class Wine < ApplicationRecord
   validates :name, uniqueness: true
+  has_many :reviews
 end
